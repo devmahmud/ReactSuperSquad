34477c9cddc1141e7a7f0cabad3be31d3c40c923
devmahmud <expelmahmud@gmail.com>
2019-10-20 21:46:23 +0600

    Updates

---
+++ precache-manifest.6089a4420a9522b21b0cb14f00c00196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cad6761ddec70b92763715bd62106b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c21445764c8d7340a8b",
+    "url": "/static/css/main.abcae048.chunk.css"
+  },
+  {
+    "revision": "da2bd359c3d9a123a931",
+    "url": "/static/js/2.f6abff62.chunk.js"
+  },
+  {
+    "revision": "9c21445764c8d7340a8b",
+    "url": "/static/js/main.e4c41a0e.chunk.js"
+  },
+  {
+    "revision": "2543dafd61b3b247e958",
+    "url": "/static/js/runtime-main.dd40047d.js"
+  },
+  {
+    "revision": "4ddc758d490102bf391845e6f0791725",
+    "url": "/static/media/background.4ddc758d.jpg"
+  }
+]);
